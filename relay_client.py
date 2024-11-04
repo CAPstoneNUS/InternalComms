dc7c5ac077ab635a819242ac2cdd1e22a62a7dff
nikkideee <nikhilwillworkwonders@gmail.com>
2024-11-04 21:57:05 +0800

    Change server sent printing to player specific laptop

--- relay_client.py
+++ relay_client.py
@@ -141,7 +141,6 @@ class RelayClient(threading.Thread):
                     # Decode and process JSON data if received completely
                     if data:
                         decoded_data = data.decode("utf-8")
-                        print(f"Server sent: {decoded_data}")
 
                         # Assuming JSON format in decoded_data
                         decoded_data = json.loads(decoded_data)
@@ -152,6 +151,7 @@ class RelayClient(threading.Thread):
                             and "player_id" in decoded_data
                         ):
                             if decoded_data["player_id"] == self.player_id:
+                                print(f"[SERVER] {decoded_data}")
                                 self.safePut(
                                     self.server_gun_state,
                                     {"bullets": decoded_data["bullets"]},
